76ee8f327723b6aad9cffb5bdc5a04aa67c4e711
tojocherian <tojocherian@gmail.com>
2018-03-18 14:14:31 +0530

    changed settings and added new methods to pipelins

--- scrapy_app/scrapy_app/pipelines.py
+++ scrapy_app/scrapy_app/pipelines.py
@@ -4,8 +4,28 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from main.models import ScrapyUnit
+import json
 
 
 class ScrapyAppPipeline(object):
+    
+    def __init__(self, unique_id, *args, **kwargs):
+        self.unique_id = unique_id
+        self.items = []
+    
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            unique_id = crawler.settings.get('unique_id'), # from django view
+        )
+    def close_spider(self, spider):
+        # dumping data to django model
+        item = ScrapyUnit()
+        item.unique_id = self.unique_id
+        item.data = json.dumps(self.items)
+        item.save()
+
     def process_item(self, item, spider):
+        self.items.append(item['url'])
         return item
--- scrapy_app/scrapy_app/settings.py
+++ scrapy_app/scrapy_app/settings.py
@@ -75,9 +75,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'scrapy_app.pipelines.ScrapyAppPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'scrapy_app.pipelines.ScrapyAppPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
