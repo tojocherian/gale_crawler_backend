2b921bf572f36586fcbfd8377cdff17e9a6abe72
tojocherian <tojocherian@gmail.com>
2018-03-18 14:01:33 +0530

    added spider class

--- scrapy_app/scrapy_app/spiders/galecrawler.py
+++ scrapy_app/scrapy_app/spiders/galecrawler.py
@@ -6,16 +6,23 @@ from scrapy.spiders import CrawlSpider, Rule
 
 class GalecrawlerSpider(CrawlSpider):
     name = 'galecrawler'
-    allowed_domains = ['https://google.com']
-    start_urls = ['http://https://google.com/']
 
-    rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
-    )
+    def __init__(self, *args, **kwargs):
+        # We are going to pass these args from our django view.
+        # To make everything dynamic, we need to override them inside __init__ method
+        self.url = kwargs.get('url')
+        self.domain = kwargs.get('domain')
+        self.start_urls = [self.url]
+        self.allowed_domains = [self.domain]
+
+        GalecrawlerSpider.rules = [
+           Rule(LinkExtractor(unique=True), callback='parse_item'),
+        ]
+        super(GalecrawlerSpider, self).__init__(*args, **kwargs)
 
     def parse_item(self, response):
+        # You can tweak each crawled page here
+        # Don't forget to return an object.
         i = {}
-        #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
-        #i['name'] = response.xpath('//div[@id="name"]').extract()
-        #i['description'] = response.xpath('//div[@id="description"]').extract()
+        i['url'] = response.url
         return i
